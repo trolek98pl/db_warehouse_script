6a8c7460a2d22a884a59d804068162660fe36987
trolek98pl <amisztal0@gmail.com>
2023-07-31 11:16:28 +0200

    Final Version

--- data_warehouse_group.py
+++ data_warehouse_group.py
@@ -1,8 +1,16 @@
 # generate student data
 
-group = ["1ED11A", "1ED11B", "1ED12A", "1ED12B", "2ED11A", "2ED11B", "2ED12A", "2ED12B"] # names of group
-group.extend(["3ED11A", "3ED11B", "3ED12A", "3ED12B", "4ED11A", "4ED11B", "4ED12A", "4ED12B"])
-group.extend(["5ED11A", "5ED11B", "5ED12A", "5ED12B"])
+# star topoplogy
+# group = ["1ED11A", "1ED11B", "1ED12A", "1ED12B", "2ED11A", "2ED11B", "2ED12A", "2ED12B"] # names of group
+# group.extend(["3ED11A", "3ED11B", "3ED12A", "3ED12B", "4ED11A", "4ED11B", "4ED12A", "4ED12B"])
+# group.extend(["5ED11A", "5ED11B", "5ED12A", "5ED12B"])
+
+#snow flake topology
+group = ["1ED11A", "1ED11B", "1ED12A", "1ED12B", "1ED13A", "1ED13B"] # names of group
+group.extend(["2ED11A", "2ED11B", "2ED12A", "2ED12B", "2ED13A", "2ED13B"])
+group.extend(["3ED11A", "3ED11B", "3ED12A", "3ED12B", "3ED13A", "3ED13B"])
+group.extend(["4ED11A", "4ED11B", "4ED12A", "4ED12B", "4ED13A", "4ED13B"])
+group.extend(["5ED11A", "5ED11B", "5ED12A", "5ED12B", "5ED13A", "5ED13B"])
 
 
 path = '/home/student/Documents/python_program/group.csv' # path to directory
@@ -12,6 +20,7 @@ f = open(path, 'w') # open a file
 index = 1   # variable needed to add 2 record with the same subject but different time
 for i in range(len(group)):    # random data and write to file (32 records)
     print(str(i + 1) + "\n")    # print value the current iteration
-    f.write(str(i + 1) + "," + group[i] + "," + "75" + "\n")    # write data in file
+    #f.write(str(i + 1) + "," + group[i] + "," + "75" + "\n")    # write data in file star topology
+    f.write(str(i + 1) + "," + group[i] + "," + "60" + "\n")    # write data in file snow flakse topology
 
 f.close() # close file
--- data_warehouse_student.py
+++ data_warehouse_student.py
@@ -1,19 +1,30 @@
 # generate student data
 import random
+import math
 
 nazwisko = ["Nowak", "Kowalski", "Wisniewski", "Wojcik", "Kowalczyk", "Kaminski", "Lewandowski"]        #   surnames
 nazwisko.extend(["Zielinski", "Szymanski", "Wozniak", "Dabrowski", "Kozlowski", "Mazur", "Jankowski"])
 nazwisko.extend(["Kwiatkowski", "Wojciechowski", "Krawczyk", "Kaczmarek", "Piotrowski", "Grabowski"])
 
 imie = ["Mateusz", "Marek", "Lukasz", "Jan", "Wojtek", "Pawel", "Jozef", "Wieslaw", "Stanislaw"]    #   names
-imie.extend(["Mariusz", "Waldemar", "Stanislaw", "Marian", "Jakub", "Maciej", "Krzysztof", "Bogdan"]);
+imie.extend(["Mariusz", "Waldemar", "Stanislaw", "Marian", "Jakub", "Maciej", "Krzysztof", "Bogdan"])
+imie.extend(["Zygfryd", "Ryszard", "Wojciech", "Boguslaw", "Rafal", "Grzegorz", "Janusz", "Antoni"]);
 
 path = '/home/student/Documents/python_program/student.csv' #   path to directory
 
 f = open(path, 'w') #   open a file
 
-for i in range(1, 1501):    #   random data and write to file (1500 records)
+#star topology
+# for i in range(1, 1501):    #   random data and write to file (1500 records)
+#     print(str(i) + "\n")    #   print value the current iteration
+#     f.write(str(i) + "," + random.choice(nazwisko) + "," + random.choice(imie) + "," + str(random.randrange(19, 26)) + "\n")    # write data in file
+
+# f.close() #close file
+
+#snow flake topology
+for i in range(1, 1801):    #   random data and write to file (1500 records)
     print(str(i) + "\n")    #   print value the current iteration
-    f.write(str(i) + "," + random.choice(nazwisko) + "," + random.choice(imie) + "," + str(random.randrange(19, 26)) + "\n")    # write data in file
+    nr_gr = int(math.ceil(i / 60))  # calculate number of group based on student index
+    f.write(str(i) + "," + str(nr_gr) + "," + random.choice(nazwisko) + "," + random.choice(imie) + "," + str(random.randrange(19, 26)) + "\n")    # write data in file
 
 f.close() #close file
